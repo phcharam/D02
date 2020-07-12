6673d9122f927a2b02008d1d6dfa5815b0a9b54b
Phillip Charamba <phcharam@c5r4s3.wethinkcode.co.za>
2020-07-12 13:21:17 +0200

    ex02

---
+++ ex02/ft_print_numbers.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_putchar(char c);
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+int main (){
+
+	char ch;
+
+	ch = '0';
+
+	while(ch <= '9')
+	{
+
+		ft_putchar(ch);
+		ch++;
+	}
+return 0;
+	}
